22174c870b57589bfed41b865395fc638aa25f11
HoangSon <hthaison17@gmail.com>
2022-09-30 11:55:35 +0700

    create project 30/9

---
+++ Website_Ecommerce.API/Program.cs
@@ -0,0 +1,41 @@
+using Website_Ecommerce.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var services = builder.Services;
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+// Add services to the container.
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+services.AddDbContext<DataContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
