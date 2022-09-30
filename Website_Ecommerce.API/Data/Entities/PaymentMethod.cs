0d14d8418638255231192e1a37878789e3eedd0b
bannhatb <vovanban184@gmail.com>
2022-09-30 14:33:16 +0700

    create database

---
+++ Website_Ecommerce.API/Data/Entities/PaymentMethod.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website_Ecommerce.API.Data.Entities
+{
+    public class PaymentMethod
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Config { get; set; }
+        public String CreateDate { get; set; }
+
+    }
+}
---
+++ Website_Ecommerce.API/Data/Entities/UserRole.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website_Ecommerce.API.Data.Entities
+{
+    public class UserRole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public List<User> users { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public List<Role> roles { get; set; }
+    }
+}
