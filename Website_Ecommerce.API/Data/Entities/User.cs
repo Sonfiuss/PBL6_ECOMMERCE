014edcee2a3ec9a266a60234e722168f6443cf1a
bannhatb <vovanban184@gmail.com>
2022-10-10 00:06:33 +0700

    update username

--- Website_Ecommerce.API/Data/Entities/User.cs
+++ Website_Ecommerce.API/Data/Entities/User.cs
@@ -10,7 +10,7 @@ namespace Website_Ecommerce.API.Data.Entities
     {
         [Key]
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public string Email { get; set; }
