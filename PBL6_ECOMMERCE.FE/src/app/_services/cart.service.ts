483423e2f25306305a789c4f18135352d5ebe059
rauxalach160695-nvt <89595822+rauxalach160695-nvt@users.noreply.github.com>
2022-12-07 11:35:20 +0700

    cart

--- PBL6_ECOMMERCE.FE/src/app/_services/cart.service.ts
+++ PBL6_ECOMMERCE.FE/src/app/_services/cart.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BusinessService } from './business.service';
+import { HttpClient } from '@angular/common/http';
+
+const API =  'https://localhost:7220'
+const GET_ALL_ITEM_BY_USER_ID = (id:any) => API + '/api/Cart/get-all-items-of-user/' + id;
+const ADD_ITEM_TO_CART = API + '/api/Cart/Add-item-to-cart' ;
+const UPDATE_ITEM_IN_CART = API + 'api/Cart/update-item-in-cart' ;
+const DELETE_ITEM_IN_CART = (id:any) => API + 'api/Cart/delete-item-in-cart/' + id;
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private businessService: BusinessService
+  ) { }
+  addItem2Cart(){
+    return this.httpClient.post(ADD_ITEM_TO_CART , this.businessService.getRequestOptions())
+  }
+  updateItem(){
+    return this.httpClient.post(UPDATE_ITEM_IN_CART, this.businessService.getRequestOptions())
+  }
+  deleteItem(id:any){
+    console.log("thinh123")
+    return this.httpClient.delete(DELETE_ITEM_IN_CART(id), this.businessService.getRequestOptions())
+  }
+  getCart(id:any){
+    console.log(id)
+    return this.httpClient.get(GET_ALL_ITEM_BY_USER_ID(id), this.businessService.getRequestOptions())
+  }
+
 }
