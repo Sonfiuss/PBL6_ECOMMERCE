692b3db5d7e4e8d8c34bb8610df45619bec7a26a
HoangSon <hthaison17@gmail.com>
2022-12-19 16:17:09 +0700

    update API Product Image

--- PBL6_ECOMMERCE.FE/src/app/pages/my-shop/my-shop.module.ts
+++ PBL6_ECOMMERCE.FE/src/app/pages/my-shop/my-shop.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MyShopRoutingModule } from './my-shop-routing.module';
+import { MyShopHeaderComponent } from './my-shop-header/my-shop-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyShopHeaderComponent
+  ],
   imports: [
     CommonModule,
     MyShopRoutingModule
