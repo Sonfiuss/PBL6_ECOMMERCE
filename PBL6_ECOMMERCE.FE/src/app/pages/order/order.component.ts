db8123908178bdf296ac6888a77fc98a91db31a2
rauxalach160695-nvt <89595822+rauxalach160695-nvt@users.noreply.github.com>
2022-12-23 03:28:42 +0700

    OrderHaveOrder

--- PBL6_ECOMMERCE.FE/src/app/pages/order/order.component.ts
+++ PBL6_ECOMMERCE.FE/src/app/pages/order/order.component.ts
@@ -1,57 +1,76 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { OrderService } from 'src/app/_services/order.service';
 import { CartService } from 'src/app/_services/cart.service';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ShopService } from 'src/app/_services/shop.service';
+import { VoucherService } from 'src/app/_services/voucher.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
+
+
 })
 export class OrderComponent implements OnInit {
 
-
+  @Input() vouchers:any
+  @Input() orderVouchers:any
   order : any;
   dataString : any;
   sumPrice : number = 0;
   address :string = "54 Nguyễn Lương Bằng, Hoà Khánh Bắc, Liên Chiểu, Đà Nẵng 550000, Việt Nam";
-  order2 :any;
+  orderSend :any;
+  target : any;
+  orderVoucherValue :number =0;
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private orderService : OrderService
+    private orderService : OrderService,
+    private modalService: NgbModal,
+    private shopService :ShopService,
+    private voucherService: VoucherService,
+    private cartService: CartService,
   ) { }
 
   ngOnInit(): void {
+    this.loadOrder()
+    this.loadSumPrice()
+  }
+
+  loadOrder(){
     this.activatedRoute.queryParams.subscribe(params => {
       let data = params;
       this.dataString = data;
       console.log(data);
       this.order = JSON.parse(this.dataString.data);
-      // console.log(this.order);
       console.log(this.order);
     });
-
-
-    // console.log(this.order[0]);
-    // this.dataString = JSON.parse(this.activatedRoute.snapshot.paramMap.get('my_order'));
+    this.loadVoucherShop()
+    this.order.map((obj:any) => {
+      obj.voucherProductId =0,
+      obj.totalPrice= obj.price*obj.amount,
+      obj.voucherProductId =0;
+      return obj;
+    })
   }
 
   createOrder(){
-    this.order2 =this.order.map((obj: any) => ({
+    this.orderSend =this.order.map((obj: any) => ({
       amount : obj.amount,
-      price : obj.price,
-      productDetailId : obj.idProductDetail
-
+      price : obj.totalPrice,
+      productDetailId : obj.idProductDetail,
+      voucherProductId : obj.voucherProductId
     }))
-    console.log(this.order2);
-    this.order2.map((obj:any) => {
-      obj.orderId=0,
-      obj.voucherProductId =1,
-      obj.note = "nothing";
-      return obj;
-    })
-    console.log(this.order);
+    console.log(this.orderSend);
+    // this.orderSend.map((obj:any) => {
+    //   obj.orderId=0,
+    //   obj.voucherProductId =0,
+    //   obj.note = "nothing";
+    //   return obj;
+    // })
+
     const submitData = {
       "id": 0,
       "state": 0,
@@ -62,10 +81,9 @@ export class OrderComponent implements OnInit {
       "voucherId": 1,
       "paymentMethodId": 0,
       "totalPrice": 3000,
-      "itemOrderDtos": this.order2
+      "itemOrderDtos": this.orderSend
 
     }
-    console.log(submitData)
     this.orderService.addOrder(submitData)
     .subscribe(
       (res:any) => {
@@ -76,6 +94,88 @@ export class OrderComponent implements OnInit {
       (err) => {
         alert("k them dc")
       })
+      this.deleteItem()
   }
 
+  openModalShopVoucher(content: any,target :any) {
+    this.target=target
+    this.order[this.target].voucherProductId = 0
+		this.modalService.open(content, { centered: true });
+    this.order[this.target].totalPrice = this.order[this.target].price *this.order[this.target].amount
+	}
+
+  openModalOrderVoucher(content: any) {
+    this.orderVoucherValue = 0
+		this.modalService.open(content, { centered: true });
+    this.loadVoucherOrder()
+	}
+
+  loadVoucherShop(){
+    this.shopService.getAllVoucherShop()
+    .subscribe(
+      (res) => this.handleGetVoucherSuccess(res),
+      (err) => this.handleGetVoucherError(err)
+    )
+  }
+
+  handleGetVoucherError(err: any){
+    console.log(err)
+  }
+  handleGetVoucherSuccess(res: any){
+    this.vouchers = res.result.data
+    console.log(res)
+  }
+
+  loadVoucherOrder(){
+    this.voucherService.getVoucherAvaiable()
+    .subscribe(
+      (res) => this.handleGetOrderVoucherSuccess(res),
+      (err) => this.handleGetOrderVoucherError(err)
+    )
+  }
+  handleGetOrderVoucherError(err: any){
+    console.log(err)
+  }
+  handleGetOrderVoucherSuccess(res: any){
+    this.orderVouchers = res.result.data
+    console.log(this.orderVouchers);
+
+  }
+
+  selectedShoprVoucher(val:any,id:any){
+    this.order[this.target].totalPrice = this.order[this.target].price *this.order[this.target].amount - val
+    this.order[this.target].voucherProductId = id;
+    this.loadSumPrice()
+  }
+  selectedOrderVoucher(val:any){
+    this.orderVoucherValue = val
+    this.loadSumPrice()
+  }
+
+  loadSumPrice(){
+    this.sumPrice =0
+    for (let i =0 ; i < this.order.length ; i++){
+      this.sumPrice += this.order[i].totalPrice  ;
+      console.log(this.sumPrice);
+
+    }
+    this.sumPrice -= this.orderVoucherValue
+  }
+  deleteItem(){
+    if(window.confirm("Ban thuc su muon xoa")){
+      for (let i=0 ; i< this.order.length; i++){
+        this.cartService.deleteItem(this.order[i].id)
+      .subscribe(
+        (res:any) => {
+
+        },
+        (err) => {
+          alert("Delete fail. Detail: " + JSON.stringify(err))
+        }
+      )
+      }
+
+    }
+
+  }
 }
