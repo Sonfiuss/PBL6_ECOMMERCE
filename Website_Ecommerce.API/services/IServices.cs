c1e50dab952783520ba3a9a78b75eea75ff90578
TrieuNQ <quytrieu1379@gmail.com>
2022-12-25 15:58:40 +0700

    feat/vnpay-payment-link

---
+++ Website_Ecommerce.API/services/IServices.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PBL6_ECOMMERCE.Website_Ecommerce.API.ModelDtos;
+using PBL6_ECOMMERCE.Website_Ecommerce.API.Response;
+using Website_Ecommerce.API.Response;
+
+namespace PBL6_ECOMMERCE.Website_Ecommerce.API.services
+{
+    public interface IServices
+    {
+        Task<Response<ResponseDefault>> getPaymentLink(int orderId,string vnp_Returnurl);
+        Task<ResponseVnPay> returnUrl(ReturnRequest request);
+    }
+}
